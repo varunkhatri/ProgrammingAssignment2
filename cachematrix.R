8db3df71f1fae6632eb4a3e44ba1653634d05880
varunkhatri <khatri_vk@yahoo.com>
2015-09-24 17:10:01 -0400

    deleted wrong file

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,25 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+		i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() {
+                x
+        }
+        setInverse<- function(inv){
+                i <<- inv 
+        }
+        
+        getInverse<- function() {
+                i
+        }
+        
+        list(set = set, get = get,
+                setInverse = setInverse,
+                getInverse = getInverse)
 }
 
 
@@ -12,4 +30,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		
+		i<- x$getInverse()
+        if(!is.null(i)){
+                message("getting cached data")
+                return(i)
+        }
+        
+        message("No cache set")
+        matrix<-x$get()
+        i<-solve(matrix, ...)
+        x$setInverse(i)
+        
+        i
 }
